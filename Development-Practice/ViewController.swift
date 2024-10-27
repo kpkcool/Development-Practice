71d400d10df2f77bc861cf6738637cbd81db9e1d
K Praveen Kumar <kpkcool@gmail.com>
2024-10-27 09:22:29 +0530

    update feature

--- Development-Practice/ViewController.swift
+++ Development-Practice/ViewController.swift
@@ -8,6 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private lazy var containerView: UIView = {
+        let view = UIView()
+        view.addCornerRadius(radius: 12)
+        view.addShadow(color: UIColor.lightGray, opacity: 0.2, radius: 4, offset: CGSize(width: 2, height: 4))
+        view.backgroundColor = .white
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
